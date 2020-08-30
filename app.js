25393147b1fbac928de3385f566657d479a78a7f
André Pinto <andrepinto.agalijo@gmail.com>
2020-08-30 21:28:01 +0200

    author routes

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 if (process.env.NODE_ENV !== 'production') {
-    console.error('here');
-    require('dotenv').load;
+    require('dotenv').config();
 }
 
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRoute = require('./routes/index');
+const authorRoute = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
-console.error(process.env.DB_URL);
-mongoose.connect('mongodb://localhost/mybrary', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true
 });
 
@@ -25,6 +26,7 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose.'));
 
-app.use(indexRoute);
+app.use('/', indexRoute);
+app.use('/authors', authorRoute);
 
 app.listen(process.env.PORT || 3000);
